66d9d1af5cebaf3675e80cded39a0ece2c44aff3
ysfakz <yusuf12pegasus@live.com>
2024-05-30 17:39:04 +0200

    added websocket connection

--- Assets/WebSocket/index.js
+++ Assets/WebSocket/index.js
@@ -1,6 +1,7 @@
-import WebSocket, { WebSocketServer } from 'ws';
+// import WebSocket, { WebSocketServer } from 'ws';
+const WebSocket = require('ws');
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
